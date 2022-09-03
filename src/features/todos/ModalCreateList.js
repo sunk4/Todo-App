161ed4a28fbee7ad39b71a4eabae35a034c85244
Roman Trnka <trnka.roman13@gmail.com>
2022-09-03 13:46:48 +0200

    added create list

--- src/features/todos/ModalCreateList.js
+++ src/features/todos/ModalCreateList.js
@@ -1,6 +1,12 @@
 import Box from '@mui/material/Box'
 import Modal from '@mui/material/Modal'
 import Button from '@mui/material/Button'
+import { useFormik } from 'formik'
+import { Input } from '@mui/material'
+import * as Yup from 'yup'
+
+import { createAsyncTodoList } from './todosSlice'
+import { useDispatch } from 'react-redux'
 
 const style = {
   position: 'absolute',
@@ -17,6 +23,23 @@ const style = {
 }
 
 const ModalCreateList = ({ open, handleClose }) => {
+  const dispatch = useDispatch()
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .max(20, 'Must be 20 characters or less')
+        .required('Required'),
+    }),
+    onSubmit: (values) => {
+      dispatch(createAsyncTodoList(values))
+      handleClose()
+    },
+  })
+
   return (
     <>
       <Modal
@@ -28,9 +51,20 @@ const ModalCreateList = ({ open, handleClose }) => {
       >
         <Box sx={{ ...style, width: 200 }}>
           <h2 id="child-modal-title">Text in a child modal</h2>
-          <p id="child-modal-description">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          </p>
+          <form onSubmit={formik.handleSubmit}>
+            <Input
+              name="name"
+              type="text"
+              placeholder="Name of Todo list"
+              value={formik.values.name || ''}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+            />
+            {formik.touched.name && formik.errors.name ? (
+              <p>{formik.errors.name}</p>
+            ) : null}
+            <Button type="submit">Submit</Button>
+          </form>
           <Button onClick={handleClose}>Close Child Modal</Button>
         </Box>
       </Modal>
